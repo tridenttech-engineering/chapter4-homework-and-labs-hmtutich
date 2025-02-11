4f44e35d846a477584c6760ebc7d83d17912df4e
hmtutich <hmtutich1@gmail.com>
2025-02-11 22:57:13 +0000

    Done with Chapter 4

--- .ccls-cache/@home@runner@workspace/hw4-16.cpp
+++ .ccls-cache/@home@runner@workspace/hw4-16.cpp
@@ -9,19 +9,22 @@ int main()
 const double increase = 1.05;
 double store1 = 0.0;
 double store2 = 0.0;
-double total = 0.0;
+//double total = 0.0;
+int store1Int = 0;
+int store2Int = 0;
+int totalInt = 0;
 cout << "Store 1 sales: ";
 cin >> store1;
 cout << "Store 2 sales: ";
 cin >> store2;
-store1 = store1 * increase;
-store2 = store2 * increase;
-total = store1 + store2;
-cout << fixed << setprecision(3) << endl;
-cout << store1 << " --->Store 1" << endl;
-cout << store2 << " --->Store 2" << endl;
+store1Int = static_cast<int>(store1 * increase * 100 + .5);
+store2Int = static_cast<int>(store2 * increase * 100 + .5);
+totalInt = store1Int + store2Int;
+cout << fixed << setprecision(2) << endl;
+cout << store1Int / 100.0 << " --->Store 1" << endl;
+cout << store2Int / 100.0 << " --->Store 2" << endl;
 cout << "-----------------" << endl;
-cout << total << " --->Total" << endl;
+cout << totalInt / 100.0 << " --->Total" << endl;
 return 0;
 } //end of main function
 //1. Declare and initalize three int variables named store1Int, store2Int, and totalInt
